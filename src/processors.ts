778701483eb5fb869dff01d65d5c8d20e3717a04
Jeremy Chone <jeremy.chone@gmail.com>
2018-06-10 16:01:23 -0700

    . pcss - remove depcreated pcss cssnext, and simplevars (we use css --var now)

--- src/processors.ts
+++ src/processors.ts
@@ -7,9 +7,7 @@ const postcss = require("postcss");
 const processors = [
 	require("postcss-import"),
 	require("postcss-mixins"),
-	require("postcss-simple-vars"),
-	require("postcss-nested"),
-	require("postcss-cssnext")({ browsers: ["last 2 versions"] })
+	require("postcss-nested")
 ];
 
 /////// for JS
